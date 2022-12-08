57a68d451ede3025e63d9530ddfe744e0217b7bd
Amir Aboueldahab <amir.aboueldahab@faktorzehn.de>
2022-12-08 10:06:10 +0100

    Day7: file has size and name

--- src/main/java/de/aboueldahab/aoc/day07/AocFile.java
+++ src/main/java/de/aboueldahab/aoc/day07/AocFile.java
@@ -2,12 +2,31 @@ package de.aboueldahab.aoc.day07;
 
 public class AocFile {
 
-//    private final String name;
-//    private final long size;
-//
-//    public AocFile(String name, long size) {
-//        this.name = name;
-//        this.size = size;
-//    }
-    
+    private String name;
+    private long size;
+
+    public AocFile() {
+
+    }
+
+    public AocFile(String name, long size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long size() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
 }
